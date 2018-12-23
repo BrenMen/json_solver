edcc756b19a16d785ec00b9831c8587af767fe1e
Brennan Minns <s195215@uos.ac.uk>
2018-12-23 20:38:57 +0000

    SendAnswerTest complete

--- src/test/java/uk/ac/uos/assignment/SendAnswerTest.java
+++ src/test/java/uk/ac/uos/assignment/SendAnswerTest.java
@@ -10,19 +10,66 @@ import org.junit.Test;
 
 public class SendAnswerTest {
 	
-	SendAnswer as;
+	SendAnswer sender;
 
 	@Before
-	public void setUp() throws Exception {
-		as = new SendAnswer();
+	public void setup() throws Exception {
+		sender = new SendAnswer();
 	}
 	
 	@Test
-	public void testingAnOverallAnswerSendProcess() throws CustomException, IOException {
-		Calculate task1 = new Calculate("add", "8054", "2119", "/answer/8473");
-		ArrayList<Calculate> listOfTasks = new ArrayList<Calculate>();
-		listOfTasks.add(task1);
-		as.sendAnswer("http://i2j.openode.io", listOfTasks);
+	public void correctCalculationForAdditionTaskTest() throws CustomException, IOException {
+		Calculate task = new Calculate("add", "8054", "2119", "/answer/8473");
+		ArrayList<Calculate> taskList = new ArrayList<Calculate>();
+		taskList.add(task);
+		String result = sender.sendAnswer("http://i2j.openode.io", taskList);
+		
+		assertEquals("Destination URL: http://i2j.openode.io/answer/8473\n"
+				+ "Instruction: add\n"
+				+ "Parameter One: 8054\n"
+				+ "Parameter Two: 2119\n"
+				+ "Answer: 10173\n\n", result);
 	}
-
+	
+	@Test
+	public void correctCalculationForMultiplicationTaskTest() throws CustomException, IOException {
+		Calculate task = new Calculate("multiply", "4500", "5245", "/answer/5589");
+		ArrayList<Calculate> taskList = new ArrayList<Calculate>();
+		taskList.add(task);
+		String result = sender.sendAnswer("http://i2j.openode.io", taskList);
+		
+		assertEquals("Destination URL: http://i2j.openode.io/answer/5589\n"
+				+ "Instruction: multiply\n"
+				+ "Parameter One: 4500\n"
+				+ "Parameter Two: 5245\n"
+				+ "Answer: 23602500\n\n", result);
+	}
+	
+	@Test
+	public void correctCalculationForConcatinationTaskTest() throws CustomException, IOException {
+		Calculate task = new Calculate("concat", "1916", "6575", "/answer/2386");
+		ArrayList<Calculate> taskList = new ArrayList<Calculate>();
+		taskList.add(task);
+		String result = sender.sendAnswer("http://i2j.openode.io", taskList);
+		
+		assertEquals("Destination URL: http://i2j.openode.io/answer/2386\n"
+				+ "Instruction: concat\n"
+				+ "Parameter One: 1916\n"
+				+ "Parameter Two: 6575\n"
+				+ "Answer: 19166575\n\n", result);
+	}
+	
+	@Test
+	public void incorrectCalculationForSubtractTaskTest() throws CustomException, IOException {
+		String customMessage = "";
+		Calculate task = new Calculate("subtract", "4918", "7706", "/answer/6160");
+		ArrayList<Calculate> taskList = new ArrayList<Calculate>();
+		taskList.add(task);
+		try {
+			sender.sendAnswer("http://i2j.openode.io", taskList);
+		} catch (CustomException error) {
+			customMessage = error.getMessage();
+		}
+		assertEquals("Error! Invalid instruction: subtract", customMessage);
+	}
 }
